13a54c3b3724f6d32ed29930eb02cff4a399a7bc
only-for-dima <dimape4enb1998@gmail.com>
2025-04-14 02:21:46 +0700

    Сделал БОЛЕЕ подробный вывод сообщения об ошибке при использовании try-except дон

--- test_main_page.py
+++ test_main_page.py
@@ -3,13 +3,17 @@ from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 
 
-
-def test_guest_can_go_to_login_page(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object,
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+#def test_guest_can_go_to_login_page(browser):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    #page = MainPage(browser, link) # инициализируем Page Object,
 # передаем в конструктор экземпляр драйвера и url адрес
-    page.open() # открываю страницу дон
-    page.go_to_login_page() # выполняю метод страницы - перехожу на страницу логина дон
+    #page.open() # открываю страницу дон
+    #page.go_to_login_page() # выполняю метод страницы - перехожу на страницу логина дон
 
 
 def go_to_login_page(browser):
